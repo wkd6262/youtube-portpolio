34e74df8cf2173834f891b77ba93eb48dc73d4df
wkd6262 <dnjswns6262@naver.com>
2024-05-28 16:38:45 +0900

    헤더 컴포넌트 화

---
+++ src/components/header/Logo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <h1 className="header_logo">
+      <a href="/">
+        <em></em>
+        <span>
+          webs
+          <br />
+          youtube
+        </span>
+      </a>
+    </h1>
+  );
+};
+
+export default Logo;
