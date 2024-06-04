83de9a3d301ec5c711a8d5d4a9d1f5e9d1526c64
wkd6262 <dnjswns6262@naver.com>
2024-06-04 16:43:00 +0900

    추천 개발자 작업

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { developerText } from "../../data/developer";
+import { Link } from "react-router-dom";
 
 const Developer = () => {
   return (
-    <div>
-      <h1>de</h1>
-    </div>
+    <section id="developer">
+      <h2>추천 개발자를 소개합니다.</h2>
+      <div className="developer_inner overflow">
+        {developerText.map((developer, key) => (
+          <div className="developer" key={key}>
+            <div className="developer_img play_icon">
+              <Link to={`/channel/${developer.channelId}`}>
+                <img src={developer.img} alt={developer.name} />
+              </Link>
+            </div>
+            <div className="developer_info">
+              <Link to={`/channel/${developer.channelId}`}>
+                {developer.name}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
